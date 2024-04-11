098e562d180a8ae1b5536a3b4ea35a905277e38e
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2024-04-11 20:07:03 +0530

    Added url dependency

--- web-server/index.js
+++ web-server/index.js
@@ -1,13 +1,19 @@
 const http = require('http')
 const fsPromises = require('fs').promises
 const path = require('path')
+const url =  require('url')
 
-const server = http.createServer(async (req,res)=>{
+async function myHandler(req,res) {
     console.log("Request Sent from the server");
-    const currLog = `${Date.now()}\t: ${req.url} New Request Received\n`
+    const reqURL = url.parse(req.url,true)
+    const currLog = `${Date.now()}\t: ${reqURL.path} ${req.method} New Request Received\n`
     await fsPromises.appendFile(path.join(__dirname,"logFile.txt"),currLog)
+    
+
     res.end("Hello Message sent from  the Server ")
-})
+}
+
+const server = http.createServer(myHandler)
 
 server.listen(8000,()=> console.log("Server Started"))
 
